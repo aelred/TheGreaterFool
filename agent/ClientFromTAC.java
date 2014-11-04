d65e28e66a6c4360f30b073e42b2fb2e1a7adc35
Harry Cutts <eternal.linux@gmail.com>
2014-11-04 12:41:14 +0000

    Make Client abstract and subclass to ClientFromTAC

---
+++ agent/ClientFromTAC.java
@@ -0,0 +1,28 @@
+package agent;
+
+import se.sics.tac.aw.TACAgent;
+
+public class ClientFromTAC extends Client {
+
+    private final TACAgent agent;
+    private final int id;
+
+    public ClientFromTAC(TACAgent agent, int id) {
+        this.agent = agent;
+        this.id = id;
+    }
+
+    public int getID() { return id; }
+
+    public int getPreferredArrivalDay() {
+        return agent.getClientPreference(id, TACAgent.ARRIVAL);
+    }
+    public int getPreferredDepartureDay() {
+        return agent.getClientPreference(id, TACAgent.DEPARTURE);
+    }
+    public int getHotelPremium() { return agent.getClientPreference(id, TACAgent.HOTEL_VALUE); }
+
+    public int getEntertainmentPremium(EntertainmentType type) {
+        return agent.getClientPreference(id, type.getTACPreferenceQueryValue());
+    }
+}
