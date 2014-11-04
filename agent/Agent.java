01dc07b9e626a5b7dcdd6b6d50d789fc944e5f99
Felix Chapman <aelred717@gmail.com>
2014-11-04 12:56:29 +0000

    Make agent create auction objects

--- agent/Agent.java
+++ agent/Agent.java
@@ -1,4 +1,5 @@
 package agent;
+import java.util.*;
 import se.sics.tac.aw.*;
 import se.sics.tac.util.ArgEnumerator;
 
@@ -9,6 +10,9 @@ public class Agent extends AgentImpl {
     public static final int NUM_CLIENTS = 8;
 
     private Client[] clients;
+    private List<Set<Auction>> flightAuctions;
+    private List<Set<Auction>> hotelAuctions;
+    private List<Set<Auction>> entertainmentAuctions;
 
     // The plane agent monitors and buys plane tickets
     private FlightAgent flightAgent;
@@ -46,6 +50,7 @@ public class Agent extends AgentImpl {
 
     public void gameStarted() {
         flightAgent = new FlightAgent();
+        createAuctions();
 
         clients = new Client[NUM_CLIENTS];
         for (int i = 0; i < clients.length; i++) {
@@ -60,5 +65,34 @@ public class Agent extends AgentImpl {
     }
 
     public void transaction(Transaction transaction) {
+    }
+
+    private void createAuctions() {
+        flightAuctions = new ArrayList<Set<Auction>>(NUM_DAYS);
+        hotelAuctions = new ArrayList<Set<Auction>>(NUM_DAYS);
+        entertainmentAuctions = new ArrayList<Set<Auction>>(NUM_DAYS);
+
+        for (int day = 0; day < NUM_DAYS; day ++) {
+            flightAuctions.add(new HashSet<Auction>());
+            hotelAuctions.add(new HashSet<Auction>());
+            entertainmentAuctions.add(new HashSet<Auction>());
+
+            if (day > 0) {
+                // No in-flights on first day
+                flightAuctions.get(day).add(new Auction(new PlaneTicket(day, false)));
+            }
+            if (day < NUM_DAYS-1) {
+                // No out-flights, hotels or entertainment on last day
+                flightAuctions.get(day).add(new Auction(new PlaneTicket(day, true)));
+
+                hotelAuctions.get(day).add(new Auction(new HotelBooking(day, false)));
+                hotelAuctions.get(day).add(new Auction(new HotelBooking(day, true)));
+
+                for (EntertainmentType type : EntertainmentType.values()) {
+                    entertainmentAuctions.get(day).add(new Auction(
+                        new EntertainmentTicket(day, type)));
+                }
+            }
+        }
     }
 }
