ed9f667eac33bd17eeebf45ac37204df247c67fc
Harry Cutts <eternal.linux@gmail.com>
2014-11-11 12:31:10 +0000

    Add beginnings of a ticket allocator

--- agent/EntertainmentAgent.java
+++ agent/EntertainmentAgent.java
@@ -16,13 +16,31 @@ public class EntertainmentAgent {
         this.tickets = tickets;
     }
 
-    public void allocateTickets() {
-        // TODO
+    protected class Allocation implements Comparable<Allocation> {
+        public final Client client;
+        public final EntertainmentTicket ticket;
+
+        public Allocation(Client client, EntertainmentTicket ticket) {
+            this.client = client;
+            this.ticket = ticket;
+        }
+
+        public int getValue() {
+            return client.getEntertainmentPremium(ticket.getType());
+        }
+
+        public int compareTo(Allocation o) {
+            return Integer.compare(this.getValue(), o.getValue());
+        }
+    }
+
+    private void displayProblem() {
         System.out.println("Clients:");
-        System.out.println("AW\tAP\tMU");
+        System.out.println("Day\tAW\tAP\tMU");
         for (int i = 0; i < clients.size(); i++) {
             Client client = clients.get(i);
-            System.out.printf("%3d\t%3d\t%3d\n",
+            System.out.printf("%d-%d\t%3d\t%3d\t%3d\n",
+                    client.getPreferredArrivalDay(), client.getPreferredDepartureDay(),
                     client.getEntertainmentPremium(EntertainmentType.ALLIGATOR_WRESTLING),
                     client.getEntertainmentPremium(EntertainmentType.AMUSEMENT),
                     client.getEntertainmentPremium(EntertainmentType.MUSEUM)
@@ -30,8 +48,58 @@ public class EntertainmentAgent {
         }
 
         System.out.println("Tickets:");
-        for (int i = 0; i < tickets.size(); i++) {
-            System.out.println(tickets.get(i));
+        for (int i = 0; i < tickets.size(); i++) { System.out.println("\t" + tickets.get(i)); }
+    }
+
+    private List<Allocation> removeMatchingAllocations(List<Allocation> allocations, Allocation addedAllocation) {
+        List<Allocation> newAllocations = new ArrayList<Allocation>();
+
+        // TODO: also remove allocations to clients who already have that entertainment
+        for (Allocation allocation : allocations) {
+            if (!((allocation.client == addedAllocation.client
+                        && allocation.ticket.getDay() == addedAllocation.ticket.getDay())
+                    || (allocation.ticket == addedAllocation.ticket))) {
+                newAllocations.add(allocation);
+            }
+        }
+
+        return newAllocations;
+    }
+
+    public void allocateTickets() {
+        displayProblem();
+
+        // Build list of possible Allocations
+        List<Allocation> allocations = new ArrayList<Allocation>();
+        for (Client client : clients) {
+            for (EntertainmentTicket ticket : tickets) {
+                if (client.getPreferredArrivalDay() <= ticket.getDay()
+                        && ticket.getDay() < client.getPreferredDepartureDay()) {
+                    // TODO: use the days we intend to give the client, not their preferences
+                    allocations.add(new Allocation(client, ticket));
+                }
+            }
+        }
+
+        // Choose the best Allocations
+        java.util.Collections.sort(allocations);
+        java.util.Collections.reverse(allocations);
+
+        System.out.println("Possible allocations:");
+        for (Allocation allocation : allocations) {
+            System.out.printf("\t%s for $%d\n", allocation.ticket, allocation.getValue());
+        }
+
+        List<Allocation> finalAllocations = new ArrayList<Allocation>();
+        while (allocations.size() > 0) {
+            Allocation addedAllocation = allocations.remove(0);
+            finalAllocations.add(addedAllocation);
+            allocations = removeMatchingAllocations(allocations, addedAllocation);
+        }
+
+        System.out.println("Final allocations:");
+        for (Allocation allocation : finalAllocations) {
+            System.out.printf("\t%s for $%d\n", allocation.ticket, allocation.getValue());
         }
     }
 
