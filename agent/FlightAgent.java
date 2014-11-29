bdf9470454ec323f2c288e0514a87d013fa30b06
Harry Cutts <eternal.linux@gmail.com>
2014-11-29 10:39:44 +0000

    Fix name errors in FlightAgent

--- agent/FlightAgent.java
+++ agent/FlightAgent.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class FlightAgent {
 
     private Set<PlaneTicket> tickets = new HashSet<PlaneTicket>();
-    private Map<PlaneTicket, PriceMonitor> monitors = new HashMap<PlaneTicket, PriceMonitor>();
+    private Map<PlaneTicket, FlightPriceMonitor> monitors = new HashMap<PlaneTicket, FlightPriceMonitor>();
 
     public FlightAgent() {
         // create a price monitor for every flight auction
         for (int day = 0; day < Agent.NUM_DAYS; day ++) {
             PlaneTicket ticketOut = new PlaneTicket(day, true);
             PlaneTicket ticketIn = new PlaneTicket(day, false);
-            monitors.put(ticketOut, new PriceMonitor(ticketOut));
-            monitors.put(ticketIn, new PriceMonitor(ticketIn));
+            monitors.put(ticketOut, new FlightPriceMonitor(ticketOut));
+            monitors.put(ticketIn, new FlightPriceMonitor(ticketIn));
         }
     }
 }
