d4876be415a1725c0ff1a16f4a163aececf50039
Ben Jesty <bj1g11@soton.ac.uk>
2014-11-29 10:38:45 +0000

    Added initial HotelAgent framework

---
+++ agent/HotelAgent.java
@@ -0,0 +1,23 @@
+package agent;
+
+import java.util.List;
+
+public class HotelAgent {
+
+	protected List<Package> packages;
+	protected boolean[] auctionsClosed = new boolean[10];
+	protected int[] ticketsHeld = new int[10];
+
+	public HotelAgent(List<Package> packages) {
+		this.packages = packages;
+	}
+
+	public void updateOffers() {
+	}
+
+	public void auctionClosed(int aucID) {
+	}
+
+	public void makeBid(int aucID, int[] offers) {
+	}
+}
