1e02e6d19b064f9c0ae03e00eebda73806c92da4
Ben Jesty <bj1g11@soton.ac.uk>
2014-12-01 13:46:25 +0000

    Merged changes

--- agent/Auction.java
+++ agent/Auction.java
@@ -32,10 +32,6 @@ public abstract class Auction {
         return agent.getAuctionFor(category, type, day);
     }
 
-    public int getDay() {
-        return day;
-    }
-
     public void addWatcher(Watcher watcher) {
         watchers.add(watcher);
     }
