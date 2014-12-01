3da434e4c0cf73dde2a8777779152bd485c624b9
Ben Jesty <bj1g11@soton.ac.uk>
2014-12-01 10:49:54 +0000

    Added support for HQW in HotelAuction

--- agent/HotelAuction.java
+++ agent/HotelAuction.java
@@ -1,17 +1,31 @@
 package agent;
 
+import agent.Auction.Watcher;
+import se.sics.tac.aw.Quote;
 import se.sics.tac.aw.TACAgent;
 
 public class HotelAuction extends Auction {
 	private boolean towers;
+	private int hqw;
 
 	public HotelAuction(TACAgent agent, int day, boolean towers) {
 		super(agent, day);
 		this.towers = towers;
+		hqw = 0;
 	}
 
 	protected int getAuctionID() {
 		return getAuctionID(TACAgent.CAT_HOTEL, 
 			towers ? TACAgent.TYPE_GOOD_HOTEL : TACAgent.TYPE_CHEAP_HOTEL);
+	}
+	
+	@Override
+	public void fireQuoteUpdated(Quote quote) {
+        hqw = quote.getHQW();
+		super.fireQuoteUpdated(quote);
+    }
+	
+	public int getHQW() {
+		return hqw;
 	}
 }
