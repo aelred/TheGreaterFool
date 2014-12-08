fc5f08c7db4bd32504634bdf308e5196ec427813
Felix Chapman <aelred717@gmail.com>
2014-12-08 13:28:42 +0000

    Fix compile script to work with new subpackages

--- compile.sh
+++ compile.sh
@@ -3,5 +3,7 @@ javac -classpath . se/sics/tac/util/*.java
 javac -classpath . se/sics/tac/aw/*.java
 javac -classpath . agent/*.java
 javac -classpath . agent/hotel/*.java
+javac -classpath . agent/flight/*.java
+javac -classpath . agent/entertainment/*.java
 javac -classpath hamcrest.jar:junit.jar:. agent/test/*.java
-jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class agent/*.class agent/hotel/*.class
+jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class agent/*.class agent/hotel/*.class agent/flight/*.class agent/entertainment/*.class
