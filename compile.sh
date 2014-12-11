3ad75fbca42a9bad63ac40a3969bbce3f370183c
Felix Chapman <aelred717@gmail.com>
2014-12-11 18:36:09 +0000

    Add logger to compile script

--- compile.sh
+++ compile.sh
@@ -5,5 +5,6 @@ javac -classpath . agent/*.java
 javac -classpath . agent/hotel/*.java
 javac -classpath . agent/flight/*.java
 javac -classpath . agent/entertainment/*.java
+javac -classpath . agent/logging/*.java
 javac -classpath hamcrest.jar:junit.jar:. agent/test/*.java
-jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class agent/*.class agent/hotel/*.class agent/flight/*.class agent/entertainment/*.class
+jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class agent/*.class agent/hotel/*.class agent/flight/*.class agent/entertainment/*.class agent/logging/*.class
